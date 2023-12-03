d80b05d1e647703647f6ad7ea44bcb3c7a3ca105
PetrosyanDev <dev.erikpetrosyan@gmail.com>
2023-12-04 00:04:29 +0400

    work.

--- sources/includes/views/MainQuiz.vue
+++ sources/includes/views/MainQuiz.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="container-fluid">
-        <!-- <div class="d-flex justify-content-center">Some</div> -->
-        <div class="d-flex justify-content-center"><QuizTimer></QuizTimer></div>
+        <div class="d-flex justify-content-center">
+            <QuizTimer></QuizTimer>
+        </div>
         <div class="row flex-nowrap">
             <div class="d-none d-xl-flex col-1 flex-fill"></div>
             <div class="col flex-fill global-container">
@@ -14,15 +15,21 @@
                             <p>{{ text }}</p>
                         </div>
                     </div>
-                    <p>Picked ->{{ q_answer }}</p>
                     <div class="mt-4 question-answer row gap-2">
                         <div v-for="(item, index) in answers" :key="item">
-                            <input type="radio" :id="(index + 1).toString()" :value="index + 1" v-model="q_answer" />
-                            <label :for="(index + 1).toString" class="col-12 btn border rounded">{{ item }} {{ index + 1 }}</label>
+                            <label v-if="q_answer === (index + 1).toString()" :for="(index + 1).toString()" class="col-12 btn border rounded border-success text-start" :style="{ 'padding-left': textsPadding }">
+                                <input type="radio" name="question" :id="(index + 1).toString()" :value="(index + 1).toString()" v-model="q_answer" style="display: none" />
+                                {{ item }}
+                            </label>
+                            <label v-else :for="(index + 1).toString()" class="col-12 btn border rounded text-start" :style="{ 'padding-left': textsPadding }">
+                                <input type="radio" name="question" :id="(index + 1).toString()" :value="(index + 1).toString()" v-model="q_answer" style="display: none" />
+                                {{ item }}
+                            </label>
+                        </div>
+                        <div v-if="q_answer !== ''" class="d-flex justify-content-center">
+                            <button @click="nextQuestion" type="button" class="col-12 col-md-6 col-lg-4 btn btn-primary mt-3">Next</button>
                         </div>
                     </div>
-
-                    <button v-if="q_answer != ''" type="button" class="btn btn-primary">Next</button>
                 </div>
             </div>
             <div class="d-none d-xl-flex col-1 flex-fill"></div>
@@ -31,12 +38,50 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import QuizTimer from './QuizTimer.vue'
 
-var q_answer = '1'
-var num = 1
-var text = 'Ո՞ր շարքի բոլոր բառերում է գրվում բ:'
-var answers = ['answer', 'answer', 'answer', 'answer']
+let q_answer = ref('')
+let num = 1
+let text = 'Ո՞ր նախադասության մեջ ուղղագրական սխալ ունեցող բառ կա:'
+let answers = [
+    '1) Տղամարդը անբարիացակամ վերաբերմունք դրսևորեց իր նոր հարևանների նկատմամբ:',
+    '2) Բանջար քաղող աղջիկների բույլը վետվետում էր լանջերն ի վեր:',
+    '3) Նա ամբողջ օրն անցկացնում էր թեթևաբարո կանանց և զեխ գինարբուքների մեջ:',
+    '4) Գեղջկուհու` արևից ու քամուց թրծված դեմքը խոսում էր նրա հոգսաշատ կյանքի մասին:'
+]
+
+const nextQuestion = () => {
+    // Handle the logic for moving to the next question
+    console.log('Selected Answer:', q_answer.value)
+
+    q_answer.value = ''
+    num += 1
+    calculatePadding()
+}
+
+var textsPadding = ref('0px')
+
+const calculatePadding = () => {
+    const maxLength = answers.reduce((max, answer) => {
+        const length = answer.length
+        return length > max ? length : max
+    }, 0)
+
+    if (maxLength > 110) {
+        textsPadding.value = '0px'
+    } else if (maxLength > 80) {
+        textsPadding.value = '20px'
+    } else if (maxLength > 50) {
+        textsPadding.value = '20px'
+    } else if (maxLength > 30) {
+        textsPadding.value = '10px'
+    } else {
+        textsPadding.value = '0px'
+    }
+}
+
+calculatePadding()
 </script>
 
 <style></style>
