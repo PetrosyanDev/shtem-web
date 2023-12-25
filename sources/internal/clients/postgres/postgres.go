cc01168b2f7bc0a2cbca5508c3152cd932b7f541
PetrosyanDev <dev.erikpetrosyan@gmail.com>
2023-12-25 19:58:26 +0400

    work.

---
+++ sources/internal/clients/postgres/postgres.go
@@ -0,0 +1,69 @@
+package postgresclient
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"shtem-web/sources/internal/configs"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+)
+
+const (
+	dbScheme = "postgresql://"
+)
+
+type PostgresDB struct {
+	*pgx.Conn
+}
+
+func (p *PostgresDB) Stop() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err := p.Conn.Close(ctx)
+	if err == nil {
+		log.Println("disconnected form PostgresDB")
+	}
+	return err
+}
+
+func NewPostgresDBConn(ctx context.Context, cfg *configs.Configs) (*PostgresDB, error) {
+	log.Println("connecting to PostgreSQL")
+	conn, err := connect(ctx, cfg)
+	if err != nil {
+		return nil, err
+	}
+	err = ping(conn)
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostgresDB{Conn: conn}, nil
+}
+
+func connect(ctx context.Context, cfg *configs.Configs) (*pgx.Conn, error) {
+	connConfig, err := pgx.ParseConfig(fmt.Sprintf(
+		"%s%s:%s/%s?user=%s&password=%s",
+		dbScheme, cfg.PostgresDB.Address, cfg.PostgresDB.Port, cfg.PostgresDB.DB, cfg.PostgresDB.User, cfg.PostgresDB.Pass,
+	))
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := pgx.ConnectConfig(ctx, connConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func ping(client *pgx.Conn) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	if err := client.Ping(ctx); err != nil {
+		return err
+	}
+	return nil
+}
--- sources/internal/configs/configs.go
+++ sources/internal/configs/configs.go
@@ -1,12 +1,46 @@
-// Erik Petrosyan ©
 package configs
 
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"os"
+)
+
 type Configs struct {
-	// TODO
+	TLS        bool
+	PostgresDB struct {
+		Address string `json:"dbAddress"`
+		Port    string `json:"dbPort"`
+		DB      string `json:"db"`
+		User    string `json:"dbUser"`
+		Pass    string `json:"dbPass"`
+		// TLS     bool   `json:"tls"`
+	} `json:"postgres"`
 }
 
 func NewConfigs(args []string) (*Configs, error) {
-	// TODO
+	cfgPath := ""
 	cfg := new(Configs)
+	for i, arg := range args {
+		if arg == "--tls" {
+			cfg.TLS = true
+		} else if arg == "--cfg" {
+			log.Println("--cfg", args[i+1])
+			if len(args) > i {
+				cfgPath = args[i+1]
+			}
+		}
+	}
+	if cfgPath == "" {
+		return nil, errors.New("config file does not specified")
+	}
+	if data, err := os.ReadFile(cfgPath); err == nil {
+		if err = json.Unmarshal(data, cfg); err != nil {
+			return nil, err
+		}
+	} else {
+		return nil, err
+	}
 	return cfg, nil
 }
