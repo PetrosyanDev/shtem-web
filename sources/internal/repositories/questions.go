47754da51f7d294e6ddd394536eca0fde9aa9237
PetrosyanDev <dev.erikpetrosyan@gmail.com>
2024-02-01 19:10:34 +0400

    work.

--- sources/internal/repositories/questions.go
+++ sources/internal/repositories/questions.go
@@ -9,8 +9,28 @@ type questionsRepository struct {
 	db postgresrepository.QuestionsDB
 }
 
-func (p *questionsRepository) GetShtemNames() ([]string, domain.Error) {
-	return p.db.GetShtemNames()
+func (p *questionsRepository) Create(question *domain.Question) domain.Error {
+	return p.db.Create(question)
+}
+
+func (p *questionsRepository) Update(question *domain.Question) domain.Error {
+	return p.db.Update(question)
+}
+
+func (p *questionsRepository) Delete(id int64) domain.Error {
+	return p.db.Delete(id)
+}
+
+func (p *questionsRepository) FindBajin(question *domain.Question) ([]*domain.Question, domain.Error) {
+	return p.db.FindBajin(question)
+}
+
+func (p *questionsRepository) FindQuestion(question *domain.Question) (*domain.Question, domain.Error) {
+	return p.db.FindQuestion(question)
+}
+
+func (p *questionsRepository) FindByID(id int) (*domain.Question, domain.Error) {
+	return p.db.FindByID(id)
 }
 
 func NewQuestionsRepository(db postgresrepository.QuestionsDB) *questionsRepository {
