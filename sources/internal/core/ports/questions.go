8878cb8eb42c95df091d3f8c73b393e44affe87c
PetrosyanDev <dev.erikpetrosyan@gmail.com>
2024-02-01 19:11:10 +0400

    work.

--- sources/internal/core/ports/questions.go
+++ sources/internal/core/ports/questions.go
@@ -3,5 +3,10 @@ package ports
 import "shtem-web/sources/internal/core/domain"
 
 type QuestionsService interface {
-	GetShtemNames() ([]string, domain.Error)
+	Create(question *domain.Question) domain.Error
+	Update(question *domain.Question) domain.Error
+	Delete(id int64) domain.Error
+	FindBajin(question *domain.Question) ([]*domain.Question, domain.Error)
+	FindQuestion(question *domain.Question) (*domain.Question, domain.Error)
+	FindByID(id int) (*domain.Question, domain.Error)
 }
--- sources/internal/core/services/questions.go
+++ sources/internal/core/services/questions.go
@@ -9,8 +9,31 @@ type questionsService struct {
 	questionsRepository repositories.QuestionsRepository
 }
 
-func (q *questionsService) GetShtemNames() ([]string, domain.Error) {
-	return q.questionsRepository.GetShtemNames()
+func (q *questionsService) Create(question *domain.Question) domain.Error {
+	err := q.questionsRepository.Create(question)
+	return err
+}
+
+func (q *questionsService) Update(question *domain.Question) domain.Error {
+	err := q.questionsRepository.Update(question)
+	return err
+}
+
+func (q *questionsService) Delete(id int64) domain.Error {
+	err := q.questionsRepository.Delete(id)
+	return err
+}
+
+func (q *questionsService) FindQuestion(question *domain.Question) (*domain.Question, domain.Error) {
+	return q.questionsRepository.FindQuestion(question)
+}
+
+func (q *questionsService) FindBajin(question *domain.Question) ([]*domain.Question, domain.Error) {
+	return q.questionsRepository.FindBajin(question)
+}
+
+func (q *questionsService) FindByID(id int) (*domain.Question, domain.Error) {
+	return q.questionsRepository.FindByID(id)
 }
 
 func NewQuestionsService(questionsRepository repositories.QuestionsRepository) *questionsService {
