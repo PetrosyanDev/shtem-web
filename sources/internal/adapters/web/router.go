2a6b772d223a5fe6465a81eb8c88357052de1d6f
PetrosyanDev <dev.erikpetrosyan@gmail.com>
2024-02-14 22:07:25 +0400

    work.

--- sources/internal/adapters/web/router.go
+++ sources/internal/adapters/web/router.go
@@ -24,7 +24,7 @@ func NewWEBRouter(handler ports.WEBHandler) *gin.Engine {
 	r.StaticFileFS("/google6cb2fd08c6c6526b.html", "google6cb2fd08c6c6526b.html", handler.Static())
 
 	r.GET("/", handler.Home("home.html"))
-	r.GET("/shtems", handler.Shtems("allShtems.html"))
+	r.GET("/shtems/", handler.Shtems("allShtems.html"))
 	r.GET("/shtems/:shtemName", handler.SingleShtem("singleShtem.html"))
 	r.GET("/shtems/:shtemName/quiz", handler.Quiz("quiz.html"))
 	r.GET("/category/:categoryName", handler.Category("singleCategory.html"))
