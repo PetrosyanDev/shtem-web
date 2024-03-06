3df5cd91c7fb1243ec5e5c12e867e21e25560e18
PetrosyanDev <dev.erikpetrosyan@gmail.com>
2024-03-06 20:45:24 +0400

    work.

--- sources/internal/adapters/web/dto/files.go
+++ sources/internal/adapters/web/dto/files.go
@@ -2,7 +2,9 @@ package dto
 
 import (
 	"bytes"
+	"fmt"
 	"net/http"
+	"shtem-web/sources/internal/core/domain"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -11,4 +13,9 @@ import (
 func WriteXMLResponse(ctx *gin.Context, name string, data *[]byte) {
 	ctx.Header("Content-Type", "application/xml; charset=utf-8")
 	http.ServeContent(ctx.Writer, ctx.Request, name, time.Now().UTC(), bytes.NewReader(*data))
+}
+
+func WriteFileResponse(ctx *gin.Context, file *domain.File) {
+	ctx.Header("Content-Disposition", fmt.Sprintf("inline; filename=\"%s\"", file.Key))
+	http.ServeContent(ctx.Writer, ctx.Request, file.Key, file.CreatedAt, bytes.NewReader(file.Bytes()))
 }
